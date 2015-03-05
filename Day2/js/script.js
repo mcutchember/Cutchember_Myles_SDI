eeda6d3b9a88212e8fe2352532ec9b19ff9fd2de
Myles Cutchember <MylesCutchember@Myless-MacBook-Pro.local>
2015-03-05 14:54:08 -0500

    Day 2 intro files
    Created .html and .js for day 2

---
+++ Day2/js/script.js
@@ -0,0 +1,19 @@
+/*
+Myles Cutchember
+March 5, 2015
+Section 00
+Day 2: Intro
+ */
+
+//alert("Testing to see if connected.");
+
+/*
+Example of a multi-line comment.
+Anything in here will not be seen by an computer.
+ */
+
+// Single line comment. ONLY this line will be ignored.
+
+// Alert boxes- Popup box that will alert the user with information.
+alert("Text that the user will see");
+
