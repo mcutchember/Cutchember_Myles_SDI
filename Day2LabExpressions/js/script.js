246da87950a09b95d4f5b50b8857a5260056da68
Myles Cutchember <MylesCutchember@Myless-MacBook-Pro.local>
2015-03-05 19:25:58 -0500

    Start of second problem
    Found out how much money I am spending on groceries each week and the
    average.

--- Day2LabExpressions/js/script.js
+++ Day2LabExpressions/js/script.js
@@ -14,9 +14,9 @@ alert("This is a test");
 
 //Declare variables
 //pizza*slices/people
-var slices = 10;
-var people = 12;
-var pizzas = 6;
+var slices = 16;
+var people = 20;
+var pizzas = 8;
 
 var slicesAte = (pizzas * slices) / people;
 
@@ -25,8 +25,19 @@ console.log("Each person ate" + " " + slicesAte + " " + "slices at pizzas at the
 
 //Slices of Pie Part 2
 
-var sparkyAte = (pizzas * slices)  - 30;
+var sparkyAte = (pizzas * slices)  % people;
 
 console.log("Sparky got" + " " + sparkyAte + " " + "slices of pizza");
 
-//
+//Average Shopping Bill
+
+var week1 = 200;
+var week2 = 150;
+var week3 = 220;
+var week4 = 200;
+var week5 = 100;
+
+var total = week1 + week2 + week3 + week4 + week5;
+var average = total / 5;
+
+console.log("You have spent a total of" +  " " + "$"+ total + " " + "on groceries over 5 weeks." + " " + "That is a average of" + " " + "$" + average + " "+"per week" );
