1949bf7f5895c1ad4d47ad06260c859a02a8b67a
Myles Cutchember <MylesCutchember@Myless-MacBook-Pro.local>
2015-03-07 15:27:40 -0500

    Learned to prompt
    Make results matter

---
+++ Day3/js/day3prompts.js
@@ -0,0 +1,32 @@
+/**
+ *Myles Cutchember
+ * March 7, 2015
+ * Section 00
+ * Day 3: Prompts
+ */
+
+//alert("Running thru the 6 wit my WOES");
+
+//Ask user for input
+//prompt("Text that the user will see")
+
+var userInput = prompt("Enter year of birth:");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+// length * width
+
+// Prompt the user for the width and height
+// Explain to them whats going on
+
+var height = prompt("Let calculate the area of a rectangle.\nPlease enter the height: ");
+
+var width = prompt("Please enter the width of the rectangle");
+
+//Calculate the area of the rectangle
+
+var area = width * height;
+
+//Tell the user the answer
+
+console.log("The area of the rectangle with a height of " + height + " and a height of " + width + " is "  + area + ".");
