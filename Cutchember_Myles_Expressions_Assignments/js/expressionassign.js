1c505fe70f9fc8414d33d7d795e6b32eeed4d05d
ysdwysd <mcutchember@fullsail.edu>
2015-03-07 22:45:53 -0500

    Testing
    Tested my code, found a error :0

--- Cutchember_Myles_Expressions_Assignments/js/expressionassign.js
+++ Cutchember_Myles_Expressions_Assignments/js/expressionassign.js
@@ -26,7 +26,6 @@ weeklyPurchased[2] = prompt("Week 3? ");
 weeklyPurchased[3] = prompt("Finally, Week 4? ");
 
 //Calculated weekly purchases
-
 var totalCost = parseInt(weeklyPurchased[0]) + parseInt(weeklyPurchased[1]) + parseInt(weeklyPurchased[2]) + parseInt(weeklyPurchased[3]) * parseInt(costofSongs);
 
 console.log("You have spent a total of "+"$"+totalCost+ " on music this month.");
@@ -36,6 +35,21 @@ totalCost *= 12;
 
 console.log("At this rate you will spend "+ "$"+totalCost+" annually.");
 
+//The testing begins
 
+//The cost of the song is $1
+//In the first week, I purchased 5 songs.
+// In the second week, I purchased 2 songs.
+// In the third week, I purchased 5 addition songs.
+//Finally, in the last week, I purchased 10 songs.
 
+//Using the Digital Music Calculator, I have spent a total of $94 on music this month.
+//If I continue to purchase music at this rate, I will spend $1128 annually.
 
+//The cost of the song is $5
+//In the first week, I purchased 1 song.
+//In the second week, I purchased 5 songs.
+//In the third week, I purchased 5 addition songs.
+//Finally, in the last week, I purchased 2 songs.
+//Using the Digital Music Calculator, I have spent a total of $21 on music this month.
+//If I continue to purchase music at this rate, I will spend $252.
