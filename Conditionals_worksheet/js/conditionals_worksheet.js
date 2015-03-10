ad1d908d285cb4a324c556f20af55a43b9ca1204
ysdwysd <mcutchember@fullsail.edu>
2015-03-10 19:51:57 -0400

    Finish second group
    Flew through the password problem:) feeling good

--- Conditionals_worksheet/js/conditionals_worksheet.js
+++ Conditionals_worksheet/js/conditionals_worksheet.js
@@ -19,7 +19,28 @@ if(unit == "C" ){
     var fahrenheit =degrees*9/5+32;
     console.log("The temperature is "+fahrenheit+" degrees Fahrenheit")
 }else{
-    var celcius = (degrees-32)*5/9;
-    console.log("The temperature is "+celcius+" degrees Celsius")
+    var celsius = (degrees-32)*5/9;
+    console.log("The temperature is "+celsius+" degrees Celsius")
 }
 
+// Check The Login
+
+//Declaring variables
+
+var userNameEntered = "Myles";
+var passwordEntered = "password";
+var correctUserName = "Myles";
+var correctPassword = "ilovecorn";
+
+// Create if statement
+
+if(correctUserName == userNameEntered && passwordEntered == correctPassword){
+    console.log("Welcome, "+correctUserName+"!")
+
+// If the user name is not correct
+}else if(userNameEntered =! correctUserName){
+    console.log("User not found. Try again")
+    //if the user name is correct but the password is not
+}else{
+    console.log("Password does not match our records.")
+}
