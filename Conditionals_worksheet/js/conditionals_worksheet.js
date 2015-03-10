8844e66573be75e62c4d6b4e12e54873b7004b7e
ysdwysd <mcutchember@fullsail.edu>
2015-03-10 19:18:54 -0400

    Edit and completed group 1
    Created formula for temperature conversion

--- Conditionals_worksheet/js/conditionals_worksheet.js
+++ Conditionals_worksheet/js/conditionals_worksheet.js
@@ -7,3 +7,19 @@
 
 //alert("Running thru the 6 wit my WOES");
 
+//Celsius to Fahrenheit converter
+
+var degrees = 70;
+var unit ="F";
+
+
+// Creating If statement using formula
+
+if(unit == "C" ){
+    var fahrenheit =degrees*9/5+32;
+    console.log("The temperature is "+fahrenheit+" degrees Fahrenheit")
+}else{
+    var celcius = (degrees-32)*5/9;
+    console.log("The temperature is "+celcius+" degrees Celsius")
+}
+
