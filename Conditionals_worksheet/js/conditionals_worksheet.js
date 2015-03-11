ffbb3ceaeb500dc870bd2bb7d5e363970748f571
ysdwysd <mcutchember@fullsail.edu>
2015-03-10 20:17:30 -0400

    Completed everything
    Last problem is solved. Good to go!

--- Conditionals_worksheet/js/conditionals_worksheet.js
+++ Conditionals_worksheet/js/conditionals_worksheet.js
@@ -30,17 +30,38 @@ if(unit == "C" ){
 var userNameEntered = "Myles";
 var passwordEntered = "password";
 var correctUserName = "Myles";
-var correctPassword = "ilovecorn";
+var correctPassword = "password";
 
 // Create if statement
 
 if(correctUserName == userNameEntered && passwordEntered == correctPassword){
-    console.log("Welcome, "+correctUserName+"!")
+    console.log("Welcome, "+correctUserName+"!");
 
 // If the user name is not correct
 }else if(userNameEntered =! correctUserName){
-    console.log("User not found. Try again")
+    console.log("User not found. Try again");
     //if the user name is correct but the password is not
 }else{
     console.log("Password does not match our records.")
 }
+
+// Movie Ticket Price
+// Declared variables
+var time= 10;
+var age = 15;
+
+var price = 12;
+var discount = 7;
+var discountPrice = price - discount;
+// If senior citizen or child - give discounted price
+if(age>=55 || age<10){
+    console.log("The ticket price is "+"$"+discountPrice)
+// If it matinee
+}else if(time>=3 && time<=5){
+    console.log("The ticket price is "+"$"+discountPrice);
+// Sorry buddy no discounts
+}else{
+    console.log("The ticket price is "+"$"+price);
+}
+
+//if((age>=55 || age<=10) || (time>=3 && time<=5))
