bc6e96be9410c2f0d285bc2768476134510f3a9a
ysdwysd <mcutchember@fullsail.edu>
2015-03-13 02:15:55 -0400

    Test my code
    Everything is good to go!

--- Cutchember_Myles_Conditionals_Assignment/js/conditionalsassignment.js
+++ Cutchember_Myles_Conditionals_Assignment/js/conditionalsassignment.js
@@ -17,7 +17,7 @@ var goodBehavior = tipRate + 5/100;
 if(people == "" || isNaN(people)){
 
     // Reprompt the user for the information
-    people = prompt("Please enter a number\nEnter how many people ate.");
+    people = prompt("Please enter a number only.\nEnter how many people ate?");
 }
 
 var cost = prompt("What is the cost of the meal?");
@@ -26,7 +26,7 @@ var cost = prompt("What is the cost of the meal?");
 if(cost == "" || isNaN(cost)){
 
     // Reprompt the user for the information
-    cost = prompt("Please enter a number\nEnter the cost of the meal?");
+    cost = prompt("Please enter a number only.\nEnter the cost of the meal?");
 }
 
 // Finding out the user experience
@@ -57,3 +57,69 @@ if(people == 1){
 
     console.log("So your just going to run out on the bill?")
 }
+
+//Testing Testing Testing
+
+/*
+Prompt: "How many people ate?"
+My input: "about twenty"
+Prompt: "Please enter a number. How may people ate?"
+ */
+
+/*
+ Prompt: "How many people ate?"
+ My input: "1"
+ Prompt: "What is the cost of the meal?"
+ My input: "cat food"
+ Prompt: "Please enter a number. What is the cost of the meal?"
+ */
+
+/*
+ Prompt: "How many people ate?"
+ My input: "1"
+ Prompt: "What is the cost of the meal?"
+ My input: "20"
+ Prompt: "Was your experience good or bad?"
+ My input: "It was aite."
+ Prompt: "Please only enter good or bad. How was your experience?"
+ */
+
+/*
+ Prompt: "How many people ate?"
+ My input: "1"
+ Prompt: "What is the cost of the meal?"
+ My input: "20"
+ Prompt: "Was your experience good or bad?"
+ My input: "good."
+ Prompt: "Based on your experience, it'll be nice to put up $4 for the tip."
+ */
+
+/*
+ Prompt: "How many people ate?"
+ My input: "1"
+ Prompt: "What is the cost of the meal?"
+ My input: "50"
+ Prompt: "Was your experience good or bad?"
+ My input: "bad."
+ Prompt: "Based on your experience, it'll be nice to put up $5 for the tip."
+ */
+
+/*
+ Prompt: "How many people ate?"
+ My input: "3"
+ Prompt: "What is the cost of the meal?"
+ My input: "85"
+ Prompt: "Was your experience good or bad?"
+ My input: "good."
+ Prompt: "Based on your experience, it'll be nice for each person to put up $6 for the tip."
+ */
+
+/*
+ Prompt: "How many people ate?"
+ My input: "8"
+ Prompt: "What is the cost of the meal?"
+ My input: "300"
+ Prompt: "Was your experience good or bad?"
+ My input: "bad"
+ Prompt: "Based on your experience, it'll be nice for each person to put up $4 for the tip."
+ */
