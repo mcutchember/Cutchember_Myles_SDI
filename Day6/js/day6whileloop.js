13648c9e50427564a0fb753e668604af9e8f9a2c
ysdwysd <mcutchember@fullsail.edu>
2015-03-14 16:34:15 -0400

    Covered While and Do While loops
    Covered While and Do While loops

--- Day6/js/day6whileloop.js
+++ Day6/js/day6whileloop.js
@@ -5,5 +5,46 @@
  * Day 6: While Loops
  */
 
-alert("Running thru the 6 wit my WOES");
+//alert("Running thru the 6 wit my WOES");
 
+// Basic While Loop
+
+// Initialize a counter variable
+
+var counter = 0;
+
+// Start a while loop
+//Condition to test goes inside parenthesis
+while(counter<200){
+    console.log("Somethings Never End");
+    console.log(counter);
+
+    // Make a change to the counter variable
+
+    counter+=50;
+
+}
+
+// Do While Loop
+// This Loop Will Run The Code First, THEN Check Condition
+
+/*
+do{
+    //Code will run
+
+} while(Condition to test) ;
+
+ */
+
+//Declare a counting variable
+
+var i = 20;
+
+do{
+
+    console.log("The number is "+i);
+
+    ///Change the counting variable
+    i++
+    
+}while(i<10);
