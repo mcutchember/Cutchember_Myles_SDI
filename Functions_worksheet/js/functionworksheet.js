2dbd440b6489f1591e899837d339b1c523e09f14
ysdwysd <mcutchember@fullsail.edu>
2015-03-17 18:16:03 -0400

    Circumference of a circle
    whipped up the code for the circumference of a circle using return

--- Functions_worksheet/js/functionworksheet.js
+++ Functions_worksheet/js/functionworksheet.js
@@ -5,5 +5,27 @@
  * Function Worksheet
  */
 
-alert("Running thru the 6 wit my WOES");
+//alert("Running thru the 6 wit my WOES");
+
+// Circumference
+
+// Calculate the circumference of a circle
+
+function circleCircum(r){
+    // C = 2*PI*r
+    var circumference = 2 * Math.PI * r;
+
+    // Return the value
+    return circumference;
+}
+
+//function call this circleArea
+// Create a variable to catch the return area
+
+var circEqual = circleCircum(6);
+
+// Console.log the results
+
+console.log("The circumference of the circle is "+circEqual);
+
 
