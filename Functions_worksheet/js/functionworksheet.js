3ea01122cd3a8c2f9d2587ed94f383196b1626e0
ysdwysd <mcutchember@fullsail.edu>
2015-03-17 18:37:37 -0400

    Finished 2nd problem
    HALL OF FAMEEEEEE HEREEEEE I COMEEEEEEEEEE!

--- Functions_worksheet/js/functionworksheet.js
+++ Functions_worksheet/js/functionworksheet.js
@@ -11,6 +11,8 @@
 
 // Calculate the circumference of a circle
 
+var radius = 6;
+
 function circleCircum(r){
     // C = 2*PI*r
     var circumference = 2 * Math.PI * r;
@@ -22,10 +24,23 @@ function circleCircum(r){
 //function call this circleArea
 // Create a variable to catch the return area
 
-var circEqual = circleCircum(6);
+var circEqual = circleCircum(radius);
 
 // Console.log the results
 
 console.log("The circumference of the circle is "+circEqual);
 
 
+// Stung!
+
+var s = 8.666666667;
+var weight = 135;
+
+function beeStings(w){
+    var stings = s * w;
+    return stings;
+}
+
+var totalStings = beeStings(weight);
+
+console.log("It takes "+Math.round(totalStings)+" bee stings to kill this animal");
