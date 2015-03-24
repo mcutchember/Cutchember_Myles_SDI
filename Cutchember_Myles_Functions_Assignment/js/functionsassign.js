c6b66b1391c40ec4f4c9b32ac71aac0dfa34ec2e
ysdwysd <mcutchember@fullsail.edu>
2015-03-23 22:57:31 -0400

    Add comments
    adding comments

--- Cutchember_Myles_Functions_Assignment/js/functionsassign.js
+++ Cutchember_Myles_Functions_Assignment/js/functionsassign.js
@@ -5,15 +5,33 @@
  * Function Assignment
  */
 
-//alert("Running thru the 6 wit my WOES");
+alert("Welcome to the letter generator\nEnter 3 words and we will tell you which word is the longest.");
 
 
+// User input 3 words, and use those 3 words are parameters.
 function longestWord(w1,w2,w3){
 
      w1 = prompt("Enter a word");
-     w2 = prompt("Enter another word");
-     w3 = prompt("Enter a final word");
+    //validate actual words are entered
+    if(w1 === "") {
+        prompt("You did not enter a word.\nPlease enter a word.");
+    }
 
+     w2 = prompt("Enter another word");
+    //validate actual words are entered
+    if(w2 === ""){
+        prompt("You did not enter a second word.\nPlease enter another word.");
+    }
+
+
+
+     w3 = prompt("Enter a final word");
+    //validate actual words are entered
+    if(w3=== ""){
+        w3 = prompt("You did not enter a final word.\nPlease enter a final word.");
+    }
+
+    // Function Arguments
     if(w1.length > w2.length && w1.length > w3.length){
         return w1;
 
@@ -26,17 +44,25 @@ function longestWord(w1,w2,w3){
 
 }
 
-
+// Calling function and assigning to variable
 var answer =longestWord();
 
 console.log("The longest work out of the 3 you entered is "+answer);
 
+// Creating anonymous function to output length of longest word.
 var howLong = function(){
 
      return answer.length
 
 };
 
+// Assigning anonymous function to variable
 var letters = howLong();
 
 console.log("It has "+letters+" letters");
+
+//
+//
+//
+//
+//
