69d2c3a2032111fb0ed6c51ad07959388110eadd
ysdwysd <mcutchember@fullsail.edu>
2015-03-25 00:01:29 -0400

    REVAMPPPP!
    Since class, I caught a better grasp of the types of functions. I
    decided to redo my assignment.

--- Cutchember_Myles_Functions_Assignment/js/functionsassign.js
+++ Cutchember_Myles_Functions_Assignment/js/functionsassign.js
@@ -5,62 +5,98 @@
  * Function Assignment
  */
 
-alert("Welcome\n Enter the first 3 words that come to mind.\nFind out which word has the most letters.");
 
+// Calculate the volume and area of a tent
 
-// User input 3 words, and use those 3 words are parameters.
-function longestWord(w1,w2,w3){
-
-     w1 = prompt("Enter a word");
-    //validate actual words are entered
-    if(w1 === "") {
-        prompt("You did not enter a word.\nPlease enter a word.");
-    }
-
-     w2 = prompt("Enter another word");
-    //validate actual words are entered
-    if(w2 === ""){
-        prompt("You did not enter a second word.\nPlease enter another word.");
-    }
-
-
-
-     w3 = prompt("Enter a final word");
-    //validate actual words are entered
-    if(w3=== ""){
-        w3 = prompt("You did not enter a final word.\nPlease enter a final word.");
-    }
-
-    // Function Arguments
-    if(w1.length > w2.length && w1.length > w3.length){
-        return w1;
-
-    }else if(w2.length > w1.length && w2.length > w3.length){
-        return w2;
-
-    }else{
-        return w3
-    }
-
+function areaTri(b,h){
+    // area =.5 * base * height
+    var a = .5*b*h;
+    return a;
 }
 
-// Calling function and assigning to variable
-var answer =longestWord();
+// Create an anon function for the volume
 
-console.log("The longest word you entered is "+answer);
-
-// Creating anonymous function to output length of longest word.
-var howLong = function(){
-
-     return answer.length
-
+var volTri = function(b,h,l){
+    // volume = .5 * (base*height) * length
+    var v = .5*(b*h)*l;
+    return v
 };
 
-// Assigning anonymous function to variable
-var letters = howLong();
 
-console.log("It has "+letters+" letters");
+// Prompt the user for width, height, and length
+var base = prompt("Going camping?\nLets figure out the area and volume of your tent\nWhats is the base?");
 
-// tray, spoon, seat ===> "the longest word you entered is spoon, it has 5 letters."
-// breath. poison, confusion ====> "the longest word you entered is confusion, it has 9 letters."
-// amoeba, island, guideline =====> "the longest word you entered is guideline, it has 9 letters."
+//Validation
+while(base === "" || isNaN(base)){
+    // Re- prompt the user
+    base = prompt("Please do not leave blank and only use numbers.\nWhat is your base?")
+}
+
+//ParseInt
+base = parseInt(base);
+
+var height = prompt("What is the height of the rectangle");
+
+//Validation
+
+while(height === "" || isNaN(height)){
+    height = prompt("Please do not leave blank and only use number\nWhat is the height?")
+}
+
+//ParseInt
+height = parseInt(height);
+
+
+var length = prompt("Lets figure out the perimeter and area of a rectangle\nWhats is the length");
+
+//Validation
+while(length === "" || isNaN(length)){
+    // Re- prompt the user
+    length = prompt("Please do not leave blank and only use numbers.\nWhat is your length.")
+}
+
+//ParseInt
+length = parseInt(length);
+
+// Function Call
+var resultArea = areaTri(base,height);
+var resultVol = volTri(base,height,length);
+
+
+//Combined console log
+console.log("The area of your tent is "+resultArea+" inches and the volume is "+resultVol+" cubic inches.");
+
+//Testing Testing Testing
+
+/*
+Tent #1
+
+Base: 15 in
+Height: 18 in
+Length: 19 in
+
+The area of your tent is 135 inches and the volume is 2565 cubic inches.
+
+
+ */
+
+/*
+ Tent #2
+
+ Base: 30 in
+ Height: 52 in
+ Length: 55 in
+
+ The area of your tent is 780 inches and the volume is 42900 cubic inches.
+
+ */
+
+/*
+ Tent #3
+
+ Base: 4in
+ Height: 3in
+ Length: 7 in
+
+The area of your tent is 6 inches and the volume is 42 cubic inches.
+ */
