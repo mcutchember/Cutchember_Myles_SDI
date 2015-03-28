9cc0e283bfbc7601f076277d7d95cd222d078918
ysdwysd <mcutchember@fullsail.edu>
2015-03-28 14:54:23 -0400

    Validation, Function, and Testing
    Completed 2nd part of final exam. Wish me luck

--- Cutchember_Myles_FinalExam/js/finalexam.js
+++ Cutchember_Myles_FinalExam/js/finalexam.js
@@ -5,5 +5,56 @@
  * Final Exam
  */
 
-alert("Running thru the 6 wit my WOES");
+//alert("Running thru the 6 wit my WOES");
 
+//Prompt the user for cost of item
+var cost = prompt("Hello, what is the cost of the item?");
+
+//Validate the prompt is in fact a number and not black
+while(cost === "" || isNaN(cost)){
+    //Re-prompt
+    cost = prompt("Please only enter a number and do not leave blank.\nWhat is the cost of the item?");
+}
+
+// Prompt the user for a discount % --- this number should be between 0-100
+var discount = prompt("What is the discounted percent?");
+
+//Validate
+while(discount < 0 && discount > 100 || isNaN(discount) || discount===""){
+    discount = prompt("Please do not leave blank, enter a number between 0 -100.\nWhat is the discount percent?");
+}
+
+function discountCost(cost,discount){
+    var calcdiscount = cost * (discount/100);
+    var discountPrice = cost - calcdiscount;
+
+    return discountPrice;
+
+}
+
+var results = discountCost(cost,discount);
+
+console.log("The final cost of an item that cost "+"$"+cost+" with a discount of "+discount+"%"+ " is $"+results+".");
+
+
+//Testing Testing Testing
+
+/*
+ Cost = $10
+ Discount = 50%
+
+ The final cost of an item that cost $10 with a discount of 50% is $5.
+
+
+
+ */
+
+/*
+ Cost = $20
+ Discount = 10%
+
+ The final cost of an item that cost $20 with a discount of 10% is $18.
+
+
+
+ */
